be2f2f946233f33969580bb2c4c9306129b143c4
aganariman <aganariman@gmail.com>
2023-08-16 18:22:32 +0200

    Added console output and sample summary as json

--- src/weather_console.py
+++ src/weather_console.py
@@ -1,13 +1,8 @@
 import weather_service
-
-# izmir weather
-# data = weather_service.getWeatherForecast(37.941113, 27.341944)
-
-# print(len(data['list']))
-
-# for item in data['list']:
-#     print(item['dt'])
-#     print(datetime.datetime.utcfromtimestamp(item['dt']).strftime('%Y-%m-%d %H:%M:%S'))
        
 data = weather_service.getFiveDayForecastSummary(37.941113, 27.341944)
-print (data)
+print("Date".ljust(12), "Temperature".ljust(14), "Precipitation(Rain)  Precipitation(Snow)")
+
+for item in data:
+    temp = '%0.0f' % data[item]["temp_max"] + "/" + '%0.0f' % data[item]["temp_min"]
+    print(data[item]["dt_txt"].ljust(12), temp.ljust(14), ('%0.0f' % data[item]["precipitation"]["rain"]).ljust(20), ('%0.0f' % data[item]["precipitation"]["snow"]).ljust(14))
