3ca988351811c5bf7d12ffb2bea5973b8e9993d2
aganariman <aganariman@gmail.com>
2023-08-16 23:24:13 +0200

    added error handling and minor refactoring

--- src/weather_console.py
+++ src/weather_console.py
@@ -7,12 +7,11 @@ zipCode = re.compile(r"^\d{5}(?:[-\s]\d{4})?$")
 def cls():
     os.system('cls' if os.name=='nt' else 'clear')
 
-cls()
-
 def __GetZipCode():
+    cls()
     zip = 0
     while True:
-        zip = input("Please enter a valid US ZipCode: ")
+        zip = input("Please enter a valid US ZipCode (Ctrl+C to exit): ")
         if zipCode.match(zip):
             break
         else:
@@ -21,15 +20,25 @@ def __GetZipCode():
     
     return zip
 
-# izmir      
-#data = weather_service.getFiveDayForecastSummary(37.941113, 27.341944)
-# den bosch
-#data = weather_service.getFiveDayForecastSummary(51.587269, 6.027147)
+def __printData(data):
+    print("Date".ljust(12), "Temperature".ljust(14), "Precipitation(Rain)  Precipitation(Snow)")
+    for item in data:
+        temp = '%0.0f' % data[item]["temp_max"] + "/" + '%0.0f' % data[item]["temp_min"]
+        print(data[item]["dt_txt"].ljust(12), temp.ljust(14), ('%0.0f mm' % data[item]["precipitation"]["rain"]).ljust(20), ('%0.0f' % data[item]["precipitation"]["snow"]).ljust(14))
 
-data = weather_service.getFiveDayForecastSummaryByZip(__GetZipCode())
+if __name__ == "__main__":
+    while True:
+        try:
+            data = weather_service.getFiveDayForecastSummaryByZip(__GetZipCode())
+        except Exception as e:
+            print('Error during retrieving forecast: ', e)
 
-print("Date".ljust(12), "Temperature".ljust(14), "Precipitation(Rain)  Precipitation(Snow)")
+        if data["isSuccess"]:
+            __printData(data["results"])
+            break
+        else:
+            print('Error from the forecast service: ', data["message"])
 
-for item in data:
-    temp = '%0.0f' % data[item]["temp_max"] + "/" + '%0.0f' % data[item]["temp_min"]
-    print(data[item]["dt_txt"].ljust(12), temp.ljust(14), ('%0.0f mm' % data[item]["precipitation"]["rain"]).ljust(20), ('%0.0f' % data[item]["precipitation"]["snow"]).ljust(14))
+        input("Press any key to enter a new zip code and try again...")
+
+    
--- src/weather_service.py
+++ src/weather_service.py
@@ -6,69 +6,77 @@ url = 'https://api.openweathermap.org/data/2.5/forecast'
 appid = 'INSERT_YOUR_API_KEY_HERE'
 units = 'metric'
 
-def getWeatherForecast(lat, lon):
+def __RequestWeatherForecast(params):
+    try:
+        resp = requests.get(url=url, params=params)
+        data = resp.json()
+        return data
+    except requests.exceptions.RequestException as e:
+        print(e.strerror) #print to log file
+        raise Exception(e)
+
+def __getWeatherForecast(lat, lon):
     params = dict(
         lat=lat,
         lon=lon,
         units=units,
         appid=appid
     )
+    return __RequestWeatherForecast(params)
 
-    resp = requests.get(url=url, params=params)
-    data = resp.json()
-
-    return data
-
-def getWeatherForecastByZip(zipcode):
+def __getWeatherForecastByZip(zipcode):
     params = dict(
         zip=zipcode,
         units=units,
         appid=appid
     )
-
-    resp = requests.get(url=url, params=params)
-    data = resp.json()
-
-    return data
+    return __RequestWeatherForecast(params)
 
 def __buildForecastSummary(fullforecast):
     summary = {}
+    summary["results"] = {}
+    summary["isSuccess"] = fullforecast["cod"] == '200'
+    summary["message"] = fullforecast["message"]
+
+    if(not summary["isSuccess"]):
+        return summary
+
     for item in fullforecast['list']:
         dt = datetime.datetime.utcfromtimestamp(item['dt'])
         shortdate = datetime.datetime(dt.year, dt. month, dt.day)
         day = str(shortdate.timestamp())
-        if not day in summary: 
-            summary[day] = {}
-            summary[day]["dt_txt"] = shortdate.strftime('%Y-%m-%d')
-            summary[day]["temp_min"] = sys.maxsize
-            summary[day]["temp_max"] = -sys.maxsize
-            summary[day]["precipitation"] = {}
-            summary[day]["precipitation"]["rain"] = 0.0
-            summary[day]["precipitation"]["snow"] = 0.0
+        if not day in summary["results"]: 
+            summary["results"][day] = {}
+            summary["results"][day]["dt_txt"] = shortdate.strftime('%Y-%m-%d')
+            summary["results"][day]["temp_min"] = sys.maxsize
+            summary["results"][day]["temp_max"] = -sys.maxsize
+            summary["results"][day]["precipitation"] = {}
+            summary["results"][day]["precipitation"]["rain"] = 0.0
+            summary["results"][day]["precipitation"]["snow"] = 0.0
 
         temp = item["main"]["temp"]
-        temp_max = summary[day]["temp_max"]
+        temp_max = summary["results"][day]["temp_max"]
         if temp > temp_max:
             temp_max = temp
 
-        if item["main"]["temp"] > summary[day]["temp_max"]:
-            summary[day]["temp_max"] = item["main"]["temp"]
+        if item["main"]["temp"] > summary["results"][day]["temp_max"]:
+            summary["results"][day]["temp_max"] = item["main"]["temp"]
 
-        if item["main"]["temp"] < summary[day]["temp_min"]:
-            summary[day]["temp_min"] = item["main"]["temp"]
+        if item["main"]["temp"] < summary["results"][day]["temp_min"]:
+            summary["results"][day]["temp_min"] = item["main"]["temp"]
 
         #item can be missing rain or snow precipation info
         if "rain" in item:
-            summary[day]["precipitation"]["rain"] += item["rain"]["3h"]
+            summary["results"][day]["precipitation"]["rain"] += item["rain"]["3h"]
 
         if "snow" in item:
-            summary[day]["precipitation"]["snow"] += item["snow"]["3h"]
+            summary["results"][day]["precipitation"]["snow"] += item["snow"]["3h"]
 
     return summary
 
 
 def getFiveDayForecastSummary(lat, lon):
-    return __buildForecastSummary(getWeatherForecast(lat, lon))
+    return __buildForecastSummary(__getWeatherForecast(lat, lon))
 
 def getFiveDayForecastSummaryByZip(zipcode):
-    return __buildForecastSummary(getWeatherForecastByZip(zipcode))
+    return __buildForecastSummary(__getWeatherForecastByZip(zipcode))
